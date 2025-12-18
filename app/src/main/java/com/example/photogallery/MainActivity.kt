eea2ef641e58871c23879124e945e9c37ae735bb
hotluner <dimkoval1@gmail.com>
2025-12-19 04:32:12 +0700

    4. replace greeting to PhotoGalleryScreen

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.photogallery.ui.theme.PhotoGalleryTheme
 
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PhotoGalleryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PhotoGalleryScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PhotoGalleryScreen(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PhotoGalleryScreenPreview() {
     PhotoGalleryTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            PhotoGalleryScreen(modifier = Modifier.padding(innerPadding))
+        }
     }
 }
