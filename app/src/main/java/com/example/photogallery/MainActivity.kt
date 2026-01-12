fb2c613aa9ee8c12fac2e0b5cf68b108e4435fff
hotluner <dimkoval1@gmail.com>
2026-01-12 23:19:33 +0700

    Start Project

---
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.photogallery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.photogallery.ui.theme.PhotoGalleryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PhotoGalleryTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PhotoGalleryScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PhotoGalleryScreen(modifier: Modifier){}
