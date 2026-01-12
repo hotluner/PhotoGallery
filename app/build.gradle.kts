00a9121c6e618eab42c736410d057738d11cc9e6
hotluner <dimkoval1@gmail.com>
2026-01-12 23:19:33 +0700

    add libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,12 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    implementation(libs.coil.compose)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
 }
